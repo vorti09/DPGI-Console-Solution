b26fce00ac8b8856fedbb38c9046bf9d877bee52
Ivan Nedorizanyuk <ivannedorizanyuk@gmail.com>
2025-05-14 15:03:10 +0300

    update Program.cs

--- DPGI-Console-Solution/Program.cs
+++ DPGI-Console-Solution/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DPGI_Console_Solution
+{
+    /// <summary>
+    /// Головний клас програми.
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Головний метод, що виконує запуск програми.
+        /// </summary>        
+        static void Main()
+        {
+        Console.WriteLine("Hello, World!");
+        }
+    }
+}
